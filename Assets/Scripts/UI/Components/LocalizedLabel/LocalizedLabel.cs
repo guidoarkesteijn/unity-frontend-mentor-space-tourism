50956c0dc573f9fb14ee1194f6bd4b1dfc6e3cfc
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2021-11-08 23:11:57 +0100

    Small changes for the build to work on WIndows and MacOSX.

--- Assets/Scripts/UI/Components/LocalizedLabel/LocalizedLabel.cs
+++ Assets/Scripts/UI/Components/LocalizedLabel/LocalizedLabel.cs
@@ -6,24 +6,24 @@ using UnityEngine.UIElements;
 public class LocalizedLabel : Label
 {
   private string Table { get; set; }
-	private string Key { get; set; }
+  private string Key { get; set; }
 
-	private Label label;
+  private Label label;
 
-	//
-	// Summary:
-	//     Instantiates a Label using the data read from a UXML file.
-	public new class UxmlFactory : UxmlFactory<LocalizedLabel, UxmlTraits>
-	{
-		public UxmlFactory() : base()
-		{
+  //
+  // Summary:
+  //     Instantiates a Label using the data read from a UXML file.
+  public new class UxmlFactory : UxmlFactory<LocalizedLabel, UxmlTraits>
+  {
+    public UxmlFactory() : base()
+    {
 
-		}
-	}
-	//
-	// Summary:
-	//     Defines UxmlTraits for the Label.
-	public new class UxmlTraits : TextElement.UxmlTraits
+    }
+  }
+  //
+  // Summary:
+  //     Defines UxmlTraits for the Label.
+  public new class UxmlTraits : TextElement.UxmlTraits
   {
     private UxmlStringAttributeDescription table = new UxmlStringAttributeDescription
     {
@@ -32,52 +32,52 @@ public class LocalizedLabel : Label
     };
 
     private UxmlStringAttributeDescription key = new UxmlStringAttributeDescription
-		{
-			name = "key",
-			defaultValue = "<KEY>"
-		};
+    {
+      name = "key",
+      defaultValue = "<KEY>"
+    };
 
-		/// <summary>
-		///   <para>Enumerator to get the child elements of the UxmlTraits of TextElement.</para>
-		/// </summary>
-		public override IEnumerable<UxmlChildElementDescription> uxmlChildElementsDescription
-		{
-			get
-			{
-				yield break;
-			}
-		}
+    /// <summary>
+    ///   <para>Enumerator to get the child elements of the UxmlTraits of TextElement.</para>
+    /// </summary>
+    public override IEnumerable<UxmlChildElementDescription> uxmlChildElementsDescription
+    {
+      get
+      {
+        yield break;
+      }
+    }
 
-		/// <summary>
-		///   <para>Initializer for the UxmlTraits for the TextElement.</para>
-		/// </summary>
-		/// <param name="ve">VisualElement to initialize.</param>
-		/// <param name="bag">Bag of attributes where to get the value from.</param>
-		/// <param name="cc">Creation Context, not used.</param>
-		public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
-		{
-			base.Init(ve, bag, cc);
-			LocalizedLabel localizedLabel = ((LocalizedLabel)ve);
-      
+    /// <summary>
+    ///   <para>Initializer for the UxmlTraits for the TextElement.</para>
+    /// </summary>
+    /// <param name="ve">VisualElement to initialize.</param>
+    /// <param name="bag">Bag of attributes where to get the value from.</param>
+    /// <param name="cc">Creation Context, not used.</param>
+    public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+    {
+      base.Init(ve, bag, cc);
+      LocalizedLabel localizedLabel = ((LocalizedLabel)ve);
+
       localizedLabel.Table = table.GetValueFromBag(bag, cc);
       localizedLabel.Key = key.GetValueFromBag(bag, cc);
 
-			localizedLabel.UpdateText();
-		}
-	}
+      localizedLabel.UpdateText();
+    }
+  }
 
-	public LocalizedLabel() : this(string.Empty, string.Empty) { }
+  public LocalizedLabel() : this(string.Empty, string.Empty) { }
 
-	public LocalizedLabel(string table, string key)
-	{
-		label = this.Q<Label>();
+  public LocalizedLabel(string table, string key)
+  {
+    label = this.Q<Label>();
 
     Table = table;
-		Key = key;
+    Key = key;
 
     RegisterCallback<AttachToPanelEvent>(OnAttachToPanelEvent);
-		RegisterCallback<DetachFromPanelEvent>(OnDetachFromPanelEvent);
-	}
+    RegisterCallback<DetachFromPanelEvent>(OnDetachFromPanelEvent);
+  }
 
   private void OnAttachToPanelEvent(AttachToPanelEvent attachToPanelEvent)
   {
@@ -86,49 +86,49 @@ public class LocalizedLabel : Label
   }
 
   private void OnDetachFromPanelEvent(DetachFromPanelEvent detachFromPanelEvent)
-	{
-		UnregisterCallback<DetachFromPanelEvent>(OnDetachFromPanelEvent);
+  {
+    UnregisterCallback<DetachFromPanelEvent>(OnDetachFromPanelEvent);
     UnregisterCallback<AttachToPanelEvent>(OnAttachToPanelEvent);
 
     Unsubscribe();
-	}
+  }
 
-	private void Subscribe()
-	{
-		if (Application.isPlaying)
-		{
-			LocalizationSettings.SelectedLocaleChanged -= OnSelectedLocaleChangedEvent;
-			LocalizationSettings.SelectedLocaleChanged += OnSelectedLocaleChangedEvent;
-		}
-	}
+  private void Subscribe()
+  {
+    if (Application.isPlaying)
+    {
+      LocalizationSettings.SelectedLocaleChanged -= OnSelectedLocaleChangedEvent;
+      LocalizationSettings.SelectedLocaleChanged += OnSelectedLocaleChangedEvent;
+    }
+  }
 
-	private void Unsubscribe()
-	{
-		if (Application.isPlaying)
-		{
-			LocalizationSettings.SelectedLocaleChanged -= OnSelectedLocaleChangedEvent;
-		}
-	}
+  private void Unsubscribe()
+  {
+    if (Application.isPlaying)
+    {
+      LocalizationSettings.SelectedLocaleChanged -= OnSelectedLocaleChangedEvent;
+    }
+  }
 
-	private void OnSelectedLocaleChangedEvent(UnityEngine.Localization.Locale obj)
-	{
-		UpdateText();
-	}
+  private void OnSelectedLocaleChangedEvent(UnityEngine.Localization.Locale obj)
+  {
+    UpdateText();
+  }
 
-	public void UpdateText()
-	{
-		string result = Table + "/" + Key;
-    
-		if(Application.isPlaying && !string.IsNullOrEmpty(Table) && !string.IsNullOrEmpty(Key))
-		{
-			result = LocalizationSettings.StringDatabase.GetLocalizedString(Table, Key);
-		}
+  public void UpdateText()
+  {
+    string result = Table + "/" + Key;
 
-		SetTextElement(result);
-	}
+    if (Application.isPlaying && !string.IsNullOrEmpty(Table) && !string.IsNullOrEmpty(Key))
+    {
+      result = LocalizationSettings.StringDatabase.GetLocalizedString(Table, Key);
+    }
 
-	private void SetTextElement(string value)
-	{
-		label.text = value;
-	}
+    SetTextElement(result);
+  }
+
+  private void SetTextElement(string value)
+  {
+    label.text = value;
+  }
 }
